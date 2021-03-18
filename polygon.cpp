1bcf8d40cf84d5ce9874f07840138357bd946d3c
Quentin Pla <quentin-pla@hotmail.fr>
2021-03-18 13:14:10 +0100

    Implémentation classe Polygone terminée

--- polygon.cpp
+++ polygon.cpp
@@ -1,14 +1,33 @@
 #include "polygon.h"
 
-Polygon::Polygon(const QVector<Point *> &points, const QColor &color) {
+Polygon::Polygon(const QVector<Point *> &points, const QColor &color, bool drawExtremities) {
     m_points = points;
     m_color = color;
+    m_drawExtremities = drawExtremities;
+
+    for (Point *point : m_points)
+        point->setColor(color);
 }
 
-void Polygon::draw(QOpenGLShaderProgram *program, QOpenGLFunctions *glFuncs) {
+void Polygon::draw(QOpenGLShaderProgram *program, QOpenGLFunctions *glFuncs,
+                   const QVector<unsigned char> &drawTypes_override) {
+    glLineWidth(3);
     GLObject::draw(program, glFuncs);
+    if (m_drawExtremities)
+        for (Point *point : m_points)
+            point->draw(program, glFuncs);
 }
 
 void Polygon::render() {
+    for (Point *point : m_points) {
+        m_vertices.append(point->getCoords());
+        m_colors.append(point->getColor());
+        m_normals.append(point->getCoords());
+    }
+    addVBO(GL_POLYGON);
     GLObject::render();
+}
+
+void Polygon::showExtremities(bool value) {
+    m_drawExtremities = value;
 }
--- polygon.h
+++ polygon.h
@@ -18,24 +18,37 @@ private:
      */
     QColor m_color;
 
+    /**
+     * Dessiner les extrémités
+     */
+    bool m_drawExtremities;
+
 public:
     /**
      * Constructeur
      * @param points points
      * @param color couleur
+     * @param drawExtremities afficher les points aux extrémités
      */
-    explicit Polygon(const QVector<Point *> &points, const QColor &color = Qt::white);
+    explicit Polygon(const QVector<Point *> &points, const QColor &color = Qt::white, bool drawExtremities = false);
 
     /**
      * Destructeur
      */
     ~Polygon() = default;
 
+    /**
+     * Afficher les extrémités du polygone
+     * @param value valeur
+     */
+    void showExtremities(bool value);
+
 protected:
 
     // OVERRIDE //
 
-    void draw(QOpenGLShaderProgram *program, QOpenGLFunctions *glFuncs) override;
+    void draw(QOpenGLShaderProgram *program, QOpenGLFunctions *glFuncs,
+              const QVector<unsigned char> &drawTypes_override = {}) override;
 
     void render() override;
 };
